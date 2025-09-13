7d4ccd7f5e171fa547c3bbe82f689012f9fa887e
klodmit <Dimaklochihin@yandex.ru>
2025-09-13 17:23:02 +0300

    done task 1.2

---
+++ l1_2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+	Написать программу, которая конкурентно рассчитает значения квадратов чисел,
+	взятых из массива [2,4,6,8,10], и выведет результаты в stdout.
+*/
+
+func main() {
+	arr := [5]int{2, 4, 6, 7, 10}
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < len(arr); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			arr[i] = arr[i] * arr[i]
+		}()
+	}
+	wg.Wait()
+	fmt.Println(arr)
+}
